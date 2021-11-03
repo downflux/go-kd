10d49872eaaf5a22cb77ebbcf5cd66a3dd6029ae
Minke Zhang <minke.zhang@gmail.com>
2021-11-02 17:15:10 -0700

    Update KD API function args to be more descriptive

--- kd/kd.go
+++ kd/kd.go
@@ -26,9 +26,9 @@ type T struct {
 	tolerance float64
 }
 
-func New(ps []point.P, tolerance float64) *T {
+func New(data []point.P, tolerance float64) *T {
 	return &T{
-		root:      node.New(ps, 0, tolerance),
+		root:      node.New(data, 0, tolerance),
 		tolerance: tolerance,
 	}
 }
@@ -44,16 +44,18 @@ func (t *T) Balance() { t.root = node.New(node.Points(t.root), 0, t.tolerance) }
 // points may unbalance the tree, causing queries to become much slower than in
 // theory. If this function must be called a number of times, it's generally
 // good practice to call Balance() in order to ensure optimal lookup times.
-func (t *T) Insert(p point.P) { t.root.Insert(p, t.tolerance) }
+func (t *T) Insert(datum point.P) { t.root.Insert(datum, t.tolerance) }
 
 // Remove deletes an existing data point from the tree. This function will
 // delete the first matching point with the given coordinates.
-func (t *T) Remove(p vector.V, f func(p point.P) bool) bool { return t.root.Remove(p, f, t.tolerance) }
+func (t *T) Remove(position vector.V, f func(datum point.P) bool) bool {
+	return t.root.Remove(position, f, t.tolerance)
+}
 
 // Filter returns a set of data points in the given bounding box. Data points
 // are added to the returned set if they fall inside the bounding box and passes
 // the given filter function.
-func Filter(t *T, r rectangle.R, f func(p point.P) bool) []point.P {
+func Filter(t *T, r rectangle.R, f func(datum point.P) bool) []point.P {
 	var data []point.P
 	for _, n := range rangesearch.Search(t.root, r) {
 		for _, p := range n.Data() {
@@ -68,13 +70,13 @@ func Filter(t *T, r rectangle.R, f func(p point.P) bool) []point.P {
 // RadialFilter returns a set of data points in the given bounding circle. Data
 // points are added to the returned set if they fall inside the bounding circle
 // and passes the given filter function.
-func RadialFilter(t *T, c circle.C, f func(p point.P) bool) []point.P {
+func RadialFilter(t *T, c circle.C, f func(datum point.P) bool) []point.P {
 	r := *rectangle.New(
 		vector.Sub(c.P(), *vector.New(c.R(), c.R())),
 		vector.Add(c.P(), *vector.New(c.R(), c.R())),
 	)
-	return Filter(t, r, func(p point.P) bool {
-		return vector.SquaredMagnitude(vector.Sub(p.P(), c.P())) <= c.R()*c.R() && f(p)
+	return Filter(t, r, func(datum point.P) bool {
+		return vector.SquaredMagnitude(vector.Sub(datum.P(), c.P())) <= c.R()*c.R() && f(datum)
 	})
 }
 
@@ -84,9 +86,9 @@ func RadialFilter(t *T, c circle.C, f func(p point.P) bool) []point.P {
 // multiple data points reside at the same spacial coordinate, this function
 // will arbitrarily return a subset of these to fulfill the k neighbors
 // criteria.
-func KNN(t *T, v vector.V, k int) []point.P {
+func KNN(t *T, position vector.V, k int) []point.P {
 	var data []point.P
-	for _, n := range knn.KNN(t.root, v, k, t.tolerance) {
+	for _, n := range knn.KNN(t.root, position, k, t.tolerance) {
 		if len(data) < k {
 			data = append(data, n.Data()...)
 		} else {
