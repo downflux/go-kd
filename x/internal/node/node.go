f0d80a120b6d1c5eb59b80f0f2d430f16d328959
Minke Zhang <minke.zhang@gmail.com>
2022-07-20 18:39:57 -0700

    Add initial node implementation

---
+++ x/internal/node/node.go
@@ -0,0 +1,103 @@
+package node
+
+import (
+	"fmt"
+
+	"github.com/downflux/go-kd/x/point"
+)
+
+type O[p point.P] struct {
+	Data []p
+	K    point.D
+	N    int
+
+	Dim  point.D
+	Low  int
+	High int
+}
+
+type N struct {
+	Low   int
+	High  int
+	Pivot int
+
+	Left  *N
+	Right *N
+}
+
+func New[p point.P](o O[p]) *N {
+	if o.Dim > o.K {
+		panic(fmt.Sprintf("given node dimension greater than vector dimension: %v > %v", o.Dim, o.K))
+	}
+	if o.High-o.Low <= o.N {
+		return &N{
+			Low:   o.Low,
+			High:  o.High,
+			Pivot: -1,
+		}
+	}
+	pivot := hoare(o.Data, o.Low, o.Low, o.High, func(a p, b p) bool { return a.P().X(o.Dim) < b.P().X(o.Dim) })
+	return &N{
+		Low:   o.Low,
+		High:  o.High,
+		Pivot: pivot,
+
+		Left: New[p](O[p]{
+			Data: o.Data,
+			Dim:  (o.Dim + 1) % o.K,
+			K:    o.K,
+			N:    o.N,
+			Low:  o.Low,
+			High: pivot - 1,
+		}),
+		Right: New[p](O[p]{
+			Data: o.Data,
+			Dim:  (o.Dim + 1) % o.K,
+			K:    o.K,
+			N:    o.N,
+			Low:  pivot + 1,
+			High: o.High,
+		}),
+	}
+}
+
+// hoare partitions the input data by the pivot.
+//
+// N.B.: The high index is exclusive -- that is, when partitioning an entire
+// array, high should be set to len(data).
+func hoare[p point.P](data []p, pivot int, low int, high int, less func(a p, b p) bool) int {
+	if pivot < 0 || low < 0 || high < 0 || pivot >= len(data) || low >= len(data) || high > len(data) {
+		return -1
+	}
+
+	// hoare partitioning requires the pivot at the beginning of the array.
+	data[pivot], data[low] = data[low], data[pivot]
+
+	// i and j are the left and right tracker indices, respectively. i is
+	// strictly increasing, while j is strictly decreasing
+	i := low + 1
+	j := high - 1
+
+	for i <= j {
+		// Skip array elements which are already sorted.
+		for ; less(data[i], data[low]) && i < j; i++ {
+		}
+		for ; less(data[low], data[j]); j-- {
+		}
+
+		if i > j {
+			break
+		}
+
+		data[i], data[j] = data[j], data[i]
+
+		i++
+		j--
+
+	}
+
+	// Since the pivot is stored at the beginning of the array, we need to
+	// do a final swap to ensure the pivot is at the right position.
+	data[low], data[i-1] = data[i-1], data[low]
+	return i - 1
+}
---
+++ x/internal/node/node_test.go
@@ -0,0 +1,209 @@
+package node
+
+import (
+	"testing"
+
+	"github.com/downflux/go-geometry/2d/vector"
+	"github.com/downflux/go-kd/x/point"
+	"github.com/downflux/go-kd/x/point/mock"
+)
+
+type cmp point.D
+
+func (f cmp) Less(a mock.P, b mock.P) bool {
+	return a.P().X(point.D(f)) < b.P().X(point.D(f))
+}
+
+func TestHoare(t *testing.T) {
+	type result struct {
+		data  []mock.P
+		pivot int
+	}
+
+	type config struct {
+		name string
+
+		data  []mock.P
+		pivot int
+		low   int
+		high  int
+		less  func(a mock.P, b mock.P) bool
+
+		want result
+	}
+
+	configs := []config{
+		{
+			name: "Trivial",
+			data: []mock.P{
+				mock.P{
+					X:    mock.V(*vector.New(100, 80)),
+					Data: "foo",
+				},
+			},
+			pivot: 0,
+			low:   0,
+			high:  1,
+			less:  cmp(point.AXIS_X).Less,
+			want: result{
+				data: []mock.P{
+					mock.P{
+						X:    mock.V(*vector.New(100, 80)),
+						Data: "foo",
+					},
+				},
+				pivot: 0,
+			},
+		},
+		{
+			name: "Simple/NoSwap",
+			data: []mock.P{
+				mock.P{
+					X:    mock.U(0),
+					Data: "foo",
+				},
+				mock.P{
+					X:    mock.U(1),
+					Data: "bar",
+				},
+			},
+			pivot: 0,
+			low:   0,
+			high:  2,
+			less:  cmp(point.AXIS_X).Less,
+			want: result{
+				data: []mock.P{
+					mock.P{
+						X:    mock.U(0),
+						Data: "foo",
+					},
+					mock.P{
+						X:    mock.U(1),
+						Data: "bar",
+					},
+				},
+				pivot: 0,
+			},
+		},
+		{
+			name: "Simple/Swap",
+			data: []mock.P{
+				mock.P{
+					X:    mock.U(1),
+					Data: "bar",
+				},
+				mock.P{
+					X:    mock.U(0),
+					Data: "foo",
+				},
+			},
+			pivot: 0,
+			low:   0,
+			high:  2,
+			less:  cmp(point.AXIS_X).Less,
+			want: result{
+				data: []mock.P{
+					mock.P{
+						X:    mock.U(0),
+						Data: "foo",
+					},
+					mock.P{
+						X:    mock.U(1),
+						Data: "bar",
+					},
+				},
+				pivot: 1,
+			},
+		},
+		{
+			name: "Pivot",
+			data: []mock.P{
+				mock.P{
+					X:    mock.U(100),
+					Data: "2",
+				},
+				mock.P{
+					X:    mock.U(0),
+					Data: "0",
+				},
+				mock.P{
+					X:    mock.U(50),
+					Data: "1",
+				},
+			},
+			pivot: 1,
+			low:   0,
+			high:  3,
+			less:  cmp(point.AXIS_X).Less,
+			want: result{
+				data: []mock.P{
+					mock.P{
+						X:    mock.U(0),
+						Data: "0",
+					},
+					mock.P{
+						X:    mock.U(100),
+						Data: "2",
+					},
+					mock.P{
+						X:    mock.U(50),
+						Data: "1",
+					},
+				},
+				pivot: 0,
+			},
+		},
+		{
+			name: "Pivot/Partial",
+			data: []mock.P{
+				mock.P{
+					X:    mock.U(100),
+					Data: "2",
+				},
+				mock.P{
+					X:    mock.U(0),
+					Data: "0",
+				},
+				mock.P{
+					X:    mock.U(50),
+					Data: "1",
+				},
+			},
+			pivot: 2,
+			low:   1,
+			high:  3,
+			less:  cmp(point.AXIS_X).Less,
+			want: result{
+				data: []mock.P{
+					mock.P{
+						X:    mock.U(100),
+						Data: "2",
+					},
+					mock.P{
+						X:    mock.U(0),
+						Data: "0",
+					},
+					mock.P{
+						X:    mock.U(50),
+						Data: "1",
+					},
+				},
+				pivot: 2,
+			},
+		},
+	}
+
+	for _, c := range configs {
+		t.Run(c.name, func(t *testing.T) {
+			if got := hoare(c.data, c.pivot, c.low, c.high, c.less); got != c.want.pivot {
+				t.Errorf("hoare() = %v, want = %v", got, c.want.pivot)
+			}
+
+			for i, got := range c.data {
+				if !mock.Equal(got, c.want.data[i]) {
+					t.Errorf("data[i] = %v, want = %v", got, c.want.data[i])
+				}
+			}
+		})
+	}
+}
