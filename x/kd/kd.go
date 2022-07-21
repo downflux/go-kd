511983f8b753cd0bc3f574b4ace19a7e27038701
Minke Zhang <minke.zhang@gmail.com>
2022-07-20 18:39:47 -0700

    Add initial node implementation

--- x/kd/kd.go
+++ x/kd/kd.go
@@ -2,6 +2,7 @@ package kd
 
 import (
 	"github.com/downflux/go-kd/x/point"
+	"github.com/downflux/go-kd/x/internal/node"
 )
 
 type O[p point.P] struct {
@@ -10,66 +11,40 @@ type O[p point.P] struct {
 	N    int
 }
 
-type N struct {
-	Low  int
-	High int
-	Mid  int
-
-	Left  *N
-	Right *N
-}
-
 type T[p point.P] struct {
 	k    point.D
 	n    int
 	data []p
+
+	root *node.N
 }
 
 func New[p point.P](o O[p]) *T[p] {
 	data := make([]p, len(o.Data))
-	if l := copy(data, o.Data); l != len(o.Data) {
+	l := copy(data, o.Data)
+	if l != len(o.Data) {
 		panic("could not copy data into k-D tree")
 	}
-	return nil
-}
-
-// hoare partitions the input data by the pivot.
-//
-// N.B.: The high index is exclusive -- that is, when partitioning an entire
-// array, high should be set to len(data).
-func hoare[p point.P](data []p, pivot int, low int, high int, less func(a p, b p) bool) int {
-	if pivot < 0 || low < 0 || high < 0 || pivot >= len(data) || low >= len(data) || high > len(data) {
-		return -1
+	if o.K < 1 {
+		panic("k-D tree must contain points with non-zero length vectors")
+	}
+	if o.N < 1 {
+		panic("k-D tree minimum leaf node size must be positive")
 	}
 
-	// hoare partitioning requires the pivot at the beginning of the array.
-	data[pivot], data[low] = data[low], data[pivot]
-
-	// i and j are the left and right tracker indices, respectively. i is
-	// strictly increasing, while j is strictly decreasing
-	i := low + 1
-	j := high - 1
-
-	for i <= j {
-		// Skip array elements which are already sorted.
-		for ; less(data[i], data[low]) && i < j; i++ {
-		}
-		for ; less(data[low], data[j]); j-- {
-		}
-
-		if i > j {
-			break
-		}
-
-		data[i], data[j] = data[j], data[i]
-
-		i++
-		j--
-
+	t := &T[p]{
+		k:    o.K,
+		n:    o.N,
+		data: data,
+		root: node.New[p](node.O[p]{
+			Data: data,
+			Dim:  0,
+			K:    o.K,
+			N:    o.N,
+			Low:  0,
+			High: len(data),
+		}),
 	}
 
-	// Since the pivot is stored at the beginning of the array, we need to
-	// do a final swap to ensure the pivot is at the right position.
-	data[low], data[i-1] = data[i-1], data[low]
-	return i - 1
+	return t
 }
