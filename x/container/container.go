15ba43377a55f22e34a5fbae06d1e2cca8df7dec
Minke Zhang <minke.zhang@gmail.com>
2022-07-29 18:34:36 -0700

    Add docstring for container API

--- x/container/container.go
+++ x/container/container.go
@@ -12,8 +12,25 @@ import (
 )
 
 type C[T point.P] interface {
+	// KNN returns the k-nearest neighbors of the given search coordinates.
+	//
+	// N.B.: KNN will return at max k neighbors; in the degenerate case that
+	// multiple data points reside at the same spacial coordinate, this
+	// function will arbitrarily return a subset of these to fulfill the
+	// k-neighbors constraint.
 	KNN(p vector.V, k int, f filter.F[T]) []T
-	RangeSearch(q hyperrectangle.R, f filter.F[T]) []T
+
+	// Data returns all data stored in the K-D tree.
 	Data() []T
+
+	// RangeSearch returns a set of data points in the given bounding box.
+	// Data points are added to the returned set if they fall inside the
+	// bounding box and passes the given filter function.
+	RangeSearch(q hyperrectangle.R, f filter.F[T]) []T
+
+	// Balance ensures the tree has minimal height by reconstructing the
+	// tree. Note that in general, mutations to the structure of the tree
+	// are expensive and complicated, so much so that it's easier to just
+	// redo the tree from scratch than to worry about shifting nodes around.
 	Balance()
 }
