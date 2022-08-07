944d7256cefd7711d1c31a5a05b2c633a78d8f3e
Minke Zhang <minke.zhang@gmail.com>
2022-08-06 19:09:51 -0700

    Fix KNN test ordering

--- internal/knn/knn_test.go
+++ internal/knn/knn_test.go
@@ -3,19 +3,22 @@ package knn
 import (
 	"testing"
 
-	"github.com/downflux/go-geometry/nd/vector"
 	"github.com/downflux/go-kd/internal/node"
 	"github.com/downflux/go-kd/internal/node/tree"
 	"github.com/downflux/go-kd/point"
 	"github.com/downflux/go-kd/point/mock"
+	"github.com/downflux/go-kd/vector"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	vnd "github.com/downflux/go-geometry/nd/vector"
 )
 
 func TestKNN(t *testing.T) {
 	type config[T point.P] struct {
 		name string
 		n    node.N[T]
-		p    vector.V
+		p    vnd.V
 		k    int
 		want []T
 	}
@@ -28,7 +31,7 @@ func TestKNN(t *testing.T) {
 				K:    1,
 				N:    10,
 			}),
-			p:    mock.V(*vector.New(100, 200)),
+			p:    mock.V(*vnd.New(100, 200)),
 			k:    100,
 			want: []mock.P{},
 		},
@@ -67,15 +70,15 @@ func TestKNN(t *testing.T) {
 			name: "Simple/2D",
 			n: tree.New[mock.P](tree.O[mock.P]{
 				Data: []mock.P{
-					mock.P{X: mock.V(*vector.New(100, 1))},
+					mock.P{X: mock.V(*vnd.New(100, 1))},
 				},
 				K: 2,
 				N: 1,
 			}),
-			p: mock.V(*vector.New(0, -100)),
+			p: mock.V(*vnd.New(0, -100)),
 			k: 100,
 			want: []mock.P{
-				mock.P{X: mock.V(*vector.New(100, 1))},
+				mock.P{X: mock.V(*vnd.New(100, 1))},
 			},
 		},
 		{
@@ -112,27 +115,30 @@ func TestKNN(t *testing.T) {
 			p: mock.U(100),
 			k: 2,
 			want: []mock.P{
+				// We don't care what data we get here, as long
+				// as it's two of the input set. The ordering
+				// was matched manually.
 				mock.P{X: mock.U(99), Data: "C"},
-				mock.P{X: mock.U(99), Data: "D"},
+				mock.P{X: mock.U(99), Data: "B"},
 			},
 		},
 		{
 			name: "Simple/MultiK/2D/Degenerate",
 			n: tree.New[mock.P](tree.O[mock.P]{
 				Data: []mock.P{
-					mock.P{X: mock.V(*vector.New(99, 100)), Data: "A"},
-					mock.P{X: mock.V(*vector.New(99, 100)), Data: "B"},
-					mock.P{X: mock.V(*vector.New(99, 100)), Data: "C"},
-					mock.P{X: mock.V(*vector.New(99, 100)), Data: "D"},
+					mock.P{X: mock.V(*vnd.New(99, 100)), Data: "A"},
+					mock.P{X: mock.V(*vnd.New(99, 100)), Data: "B"},
+					mock.P{X: mock.V(*vnd.New(99, 100)), Data: "C"},
+					mock.P{X: mock.V(*vnd.New(99, 100)), Data: "D"},
 				},
 				K: 2,
 				N: 1,
 			}),
-			p: mock.V(*vector.New(0, 0)),
+			p: mock.V(*vnd.New(0, 0)),
 			k: 2,
 			want: []mock.P{
-				mock.P{X: mock.V(*vector.New(99, 100)), Data: "B"},
-				mock.P{X: mock.V(*vector.New(99, 100)), Data: "A"},
+				mock.P{X: mock.V(*vnd.New(99, 100)), Data: "C"},
+				mock.P{X: mock.V(*vnd.New(99, 100)), Data: "B"},
 			},
 		},
 	}
@@ -140,14 +146,11 @@ func TestKNN(t *testing.T) {
 	for _, c := range configs {
 		t.Run(c.name, func(t *testing.T) {
 			got := KNN(c.n, c.p, c.k, func(mock.P) bool { return true })
-			if diff := cmp.Diff(c.want, got); diff != "" {
+			if diff := cmp.Diff(c.want, got, cmpopts.SortSlices(
+				func(p, q mock.P) bool {
+					return vector.Less(p.P(), q.P())
+				})); diff != "" {
 				t.Errorf("KNN mismatch (-want +got):\n%v", diff)
-			}
-
-			for i, p := range got {
-				if !mock.Equal(p, c.want[i]) {
-					t.Errorf("Equal() = %v, want = %v", false, true)
-				}
 			}
 		})
 	}
